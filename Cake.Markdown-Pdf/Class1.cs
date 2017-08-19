6d6e22bac9da100513134581d332283d7ffd6e5e
wozzo <w.n.bates@gmail.com>
2017-08-19 08:53:59 +0100

    Add project files.

---
+++ Cake.Markdown-Pdf/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cake.Markdown_Pdf
+{
+    public class Class1
+    {
+    }
+}
